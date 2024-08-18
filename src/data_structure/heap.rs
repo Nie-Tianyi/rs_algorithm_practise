4214014247d5b79432df0dc1b8124675beb04147
Nie-Tianyi <1355705558@qq.com>
2024-08-18 23:14:56 +0800

    feat: add new method and struct MaxHep & MinHeap

--- src/data_structure/heap.rs
+++ src/data_structure/heap.rs
@@ -1,16 +1,76 @@
-pub struct Heap<T> {
+pub struct MaxHeap<T> {
     data: Vec<T>,
 }
 
-impl<T> Heap<T> {
+impl<T: Ord> MaxHeap<T> {
     #[inline]
-    pub fn new() -> Heap<T> {
+    pub fn new() -> MaxHeap<T> {
+        Self { data: Vec::new() }
+    }
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+    #[inline]
+    pub fn peek(&self) -> Option<&T> {
+        self.data.get(0)
+    }
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+    #[inline]
+    pub fn into_inner(self) -> Vec<T> {
+        self.data
+    }
+}
+
+impl<T> Default for MaxHeap<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+// alias for MinHeap
+pub type PriorityQueue<T> = MinHeap<T>;
+
+pub struct MinHeap<T> {
+    data: Vec<T>,
+}
+
+impl<T: Ord> MinHeap<T> {
+    #[inline]
+    pub fn new() -> MinHeap<T> {
+        Self { data: Vec::new() }
+    }
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+    #[inline]
+    pub fn peek(&self) -> Option<&T> {
+        self.data.get(0)
+    }
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+    #[inline]
+    pub fn into_inner(self) -> Vec<T> {
+        self.data
+    }
+}
+
+impl<T> Default for MinHeap<T> {
+    fn default() -> Self {
         Self { data: Vec::new() }
     }
 }
 
-impl<T> Default for Heap<T> {
-    fn default() -> Self {
-        Self::new()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_new() {
+        let pq = PriorityQueue::new();
     }
 }
