3e55d6979e68b1c237db6a3fdc71c784a1e323b4
nty-mac <1355705558@qq..com>
2024-08-19 12:42:53 +0800

    refactor: change Default Implementation to derive

--- src/data_structure/heap.rs
+++ src/data_structure/heap.rs
@@ -1,3 +1,4 @@
+#[derive(Default, Clone)]
 pub struct MaxHeap<T> {
     data: Vec<T>,
 }
@@ -13,26 +14,21 @@ impl<T: Ord> MaxHeap<T> {
     }
     #[inline]
     pub fn peek(&self) -> Option<&T> {
-        self.data.get(0)
+        self.data.first()
     }
     #[inline]
     pub fn is_empty(&self) -> bool {
-        self.is_empty()
+        self.data.is_empty()
     }
     #[inline]
     pub fn into_inner(self) -> Vec<T> {
         self.data
     }
 }
-
-impl<T> Default for MaxHeap<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 // alias for MinHeap
 pub type PriorityQueue<T> = MinHeap<T>;
 
+#[derive(Clone, Default)]
 pub struct MinHeap<T> {
     data: Vec<T>,
 }
@@ -48,21 +44,15 @@ impl<T: Ord> MinHeap<T> {
     }
     #[inline]
     pub fn peek(&self) -> Option<&T> {
-        self.data.get(0)
+        self.data.first()
     }
     #[inline]
     pub fn is_empty(&self) -> bool {
-        self.is_empty()
+        self.data.is_empty()
     }
     #[inline]
     pub fn into_inner(self) -> Vec<T> {
         self.data
-    }
-}
-
-impl<T> Default for MinHeap<T> {
-    fn default() -> Self {
-        Self { data: Vec::new() }
     }
 }
 
@@ -71,6 +61,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_new() {
-        let pq = PriorityQueue::new();
+        let _pq = PriorityQueue::<i32>::new();
     }
 }
