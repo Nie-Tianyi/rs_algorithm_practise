9efe39490a069f53a1a8b9798a2a2af85e58e198
nty-mac <1355705558@qq..com>
2024-08-25 20:27:38 +0800

    refactor: make Heap struct to Generics

--- src/data_structure/heap.rs
+++ src/data_structure/heap.rs
@@ -1,58 +1,51 @@
-#[derive(Default, Debug, Clone, PartialEq)]
-pub struct MaxHeap<T> {
+pub struct Max;
+pub struct Min;
+
+pub type MaxHeap<T> = Heap<T, Max>;
+pub type PriorityQueue<T> = Heap<T, Min>;
+
+#[derive(Debug, Default, Clone)]
+pub struct Heap<T: PartialOrd, Marker = Min> {
+    marker: std::marker::PhantomData<Marker>,
     data: Vec<T>,
 }
 
-impl<T: Ord> MaxHeap<T> {
+impl<T: PartialOrd, Marker> Heap<T, Marker> {
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+    #[inline]
+    pub fn peek(&self) -> Option<&T> {
+        self.data.first()
+    }
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+    #[inline]
+    pub fn into_inner(self) -> Vec<T> {
+        self.data
+    }
+}
+
+impl<T: PartialOrd> MaxHeap<T> {
     #[inline]
     pub fn new() -> MaxHeap<T> {
-        Self { data: Vec::new() }
-    }
-    #[inline]
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-    #[inline]
-    pub fn peek(&self) -> Option<&T> {
-        self.data.first()
-    }
-    #[inline]
-    pub fn is_empty(&self) -> bool {
-        self.data.is_empty()
-    }
-    #[inline]
-    pub fn into_inner(self) -> Vec<T> {
-        self.data
+        MaxHeap {
+            marker: Default::default(),
+            data: Vec::new(),
+        }
     }
 }
-// alias for MinHeap
-pub type PriorityQueue<T> = MinHeap<T>;
 
-#[derive(Default, Debug, Clone, PartialEq)]
-pub struct MinHeap<T> {
-    data: Vec<T>,
-}
-
-impl<T: Ord> MinHeap<T> {
+impl<T: PartialOrd> PriorityQueue<T> {
     #[inline]
-    pub fn new() -> MinHeap<T> {
-        Self { data: Vec::new() }
-    }
-    #[inline]
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-    #[inline]
-    pub fn peek(&self) -> Option<&T> {
-        self.data.first()
-    }
-    #[inline]
-    pub fn is_empty(&self) -> bool {
-        self.data.is_empty()
-    }
-    #[inline]
-    pub fn into_inner(self) -> Vec<T> {
-        self.data
+    pub fn new() -> PriorityQueue<T> {
+        PriorityQueue {
+            marker: Default::default(),
+            data: Vec::new(),
+        }
     }
 }
 
@@ -61,6 +54,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_new() {
-        let _pq = PriorityQueue::<i32>::new();
+        let pq = PriorityQueue::<i32>::new();
+        println!("{}", pq.len());
     }
 }
