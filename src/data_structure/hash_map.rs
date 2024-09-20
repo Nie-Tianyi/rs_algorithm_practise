2f951cfaf645e3ac3bad1bbdb8da689535f87737
= <=>
2024-09-20 16:41:03 +0800

    ✨ feat(hash_map.rs): add struct HashMap
    Add a new struct HashMap and its relevant methds insert get get_mut

--- src/data_structure/hash_map.rs
+++ src/data_structure/hash_map.rs
@@ -2,15 +2,17 @@ use crate::supplementary::hash::hash;
 use std::borrow::Borrow;
 use std::hash::Hash;
 
-#[derive(Debug)]
-pub struct BucketList<K, V> {
+/// This is an internal data structure of a HashMap
+#[derive(Debug, Clone, Default)]
+struct BucketList<K, V> {
     seed: u64,
     len: usize,
     buckets: Vec<Vec<(K, V)>>,
 }
 
 impl<K: Hash + Eq, V> BucketList<K, V> {
-    pub fn new() -> Self {
+    #[inline]
+    fn new() -> Self {
         BucketList {
             seed: rand::random(),
             len: 0,
@@ -19,14 +21,14 @@ impl<K: Hash + Eq, V> BucketList<K, V> {
     }
 
     // return an usize indicating whether a bucket is too full
-    pub fn push(&mut self, key: K, value: V) -> usize {
+    fn push(&mut self, key: K, value: V) -> usize {
         let index = hash(self.seed, &key) as usize % self.buckets.len();
         self.buckets[index].push((key, value));
         self.len += 1;
         self.buckets[index].len()
     }
 
-    pub fn get<KB>(&self, key: &KB) -> Option<&V>
+    fn get<KB>(&self, key: &KB) -> Option<&V>
     where
         K: Borrow<KB>,
         KB: Hash + Eq + ?Sized,
@@ -40,7 +42,7 @@ impl<K: Hash + Eq, V> BucketList<K, V> {
         None
     }
 
-    pub fn get_mut<KB>(&mut self, key: &KB) -> Option<&mut V>
+    fn get_mut<KB>(&mut self, key: &KB) -> Option<&mut V>
     where
         K: Borrow<KB>,
         KB: Hash + Eq + ?Sized,
@@ -54,7 +56,7 @@ impl<K: Hash + Eq, V> BucketList<K, V> {
         None
     }
 
-    pub fn bucket(&mut self, index: usize) -> Option<Vec<(K, V)>> {
+    fn bucket(&mut self, index: usize) -> Option<Vec<(K, V)>> {
         if index >= self.buckets.len() {
             return None;
         }
@@ -63,20 +65,22 @@ impl<K: Hash + Eq, V> BucketList<K, V> {
         self.len -= res.len();
         Some(res)
     }
-}
 
-impl<K: Hash + Eq, V> Default for BucketList<K, V> {
-    fn default() -> Self {
-        Self::new()
+    /// grow the buckets to a given size.
+    #[inline]
+    fn set_buckets(&mut self, n: usize) {
+        for _ in self.buckets.len()..n {
+            self.buckets.push(Vec::new());
+        }
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod bucketlist_tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_bucketlist() {
         let mut bl = BucketList::new();
 
         bl.push("port", 8080);
@@ -84,5 +88,98 @@ mod tests {
 
         *bl.get_mut("port").unwrap() += 1;
         assert_eq!(bl.get("port"), Some(&8081));
+    }
+}
+
+const BUCKET_SIZE: usize = 8;
+const GROW_SIZE: usize = 8;
+
+#[derive(Debug, Default, Clone)]
+pub struct HashMap<K, V> {
+    n_moved: usize,
+    main: BucketList<K, V>,
+    grow: BucketList<K, V>,
+}
+
+impl<K: Hash + Eq, V> HashMap<K, V> {
+    #[inline]
+    pub fn new() -> Self {
+        HashMap {
+            n_moved: 0,
+            main: BucketList::new(),
+            grow: BucketList::new(),
+        }
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        // check if the key already exists, mutate orginal value
+        if let Some(iv) = self.main.get_mut(&key) {
+            *iv = value;
+            return;
+        }
+        if let Some(iv) = self.grow.get_mut(&key) {
+            *iv = value;
+            return;
+        }
+
+        if self.n_moved > 0 {
+            self.grow.push(key, value);
+            self.move_buckets();
+            return;
+        }
+        if self.main.push(key, value) > BUCKET_SIZE / 2 {
+            // grow buckets
+            self.move_buckets();
+        }
+    }
+
+    pub fn move_buckets(&mut self) {
+        if self.n_moved == 0 {
+            self.grow.set_buckets(self.main.buckets.len() + GROW_SIZE);
+        }
+        if let Some(b) = self.main.bucket(self.n_moved) {
+            for (k, v) in b {
+                self.grow.push(k, v);
+            }
+            self.n_moved += 1;
+            return;
+        }
+        std::mem::swap(&mut self.main, &mut self.grow);
+        self.n_moved = 0;
+    }
+
+    #[inline]
+    pub fn get<KR>(&self, kr: &KR) -> Option<&V>
+    where
+        K: Borrow<KR>,
+        KR: Hash + Eq + ?Sized,
+    {
+        self.main.get(kr).or_else(|| self.grow.get(kr))
+    }
+
+    #[inline]
+    pub fn get_mut<KR>(&mut self, kr: &KR) -> Option<&mut V>
+    where
+        K: Borrow<KR>,
+        KR: Hash + Eq + ?Sized,
+    {
+        self.main.get_mut(kr).or_else(|| self.grow.get_mut(kr))
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.main.len + self.grow.len
+    }
+}
+
+#[cfg(test)]
+mod hashmap_test {
+    use super::*;
+
+    #[test]
+    fn test_hashmap() {
+        let mut hm = HashMap::new();
+        hm.insert("key", "value");
+        assert_eq!(1 + 1, 2);
     }
 }
